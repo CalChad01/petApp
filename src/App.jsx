86e9fd629660bf98e9dbc4d45a7442c51939f0e2
Jixson <78332691+Jixson1@users.noreply.github.com>
2023-01-30 13:20:06 -0600

    install react-router-dom & update README

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div>
-      howdy world
+      
     </div>
   );
 }
